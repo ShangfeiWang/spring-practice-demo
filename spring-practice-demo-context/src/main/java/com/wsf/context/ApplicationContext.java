a105bc62772fee18c28079e510db2d83de6e2fc6
wsf <wsf@servyou.com.cn>
2023-01-05 15:49:53 +0800

    测试RequestParam注解

--- spring-practice-demo-context/src/main/java/com/wsf/context/ApplicationContext.java
+++ spring-practice-demo-context/src/main/java/com/wsf/context/ApplicationContext.java
@@ -1,17 +1,22 @@
 package com.wsf.context;
 
-import com.wsf.context.service.UserService;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.wsf.context.config.InterceptorConfig;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 public class ApplicationContext {
 
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        UserService bean = applicationContext.getBean(UserService.class);
+        //ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        //UserService bean = applicationContext.getBean(UserService.class);
+        //System.out.println(bean);
+
+        AnnotationConfigServletWebServerApplicationContext applicationContext1 = new AnnotationConfigServletWebServerApplicationContext(InterceptorConfig.class);
+        applicationContext1.start();
+        StringRedisTemplate bean = applicationContext1.getBean(StringRedisTemplate.class);
         System.out.println(bean);
-
-
+        bean.opsForValue().set("k1", "v1");
 
     }
 }
--- spring-practice-demo-context/src/main/java/com/wsf/context/controller/HelloController.java
+++ spring-practice-demo-context/src/main/java/com/wsf/context/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.wsf.context.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.LastModified;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotEmpty;
 
 @RestController
 public class HelloController implements LastModified {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @ResponseBody
     @RequestMapping("/hello")
@@ -17,6 +25,12 @@ public class HelloController implements LastModified {
         return "hello";
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/testRequestParam", method = RequestMethod.GET)
+    public String testRequestParam(@RequestParam @NotEmpty String name) {
+        System.out.println("hello.....");
+        return "hello";
+    }
 
     @Override
     public long getLastModified(HttpServletRequest request) {
