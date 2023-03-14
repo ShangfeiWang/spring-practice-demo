cd824ca6289344b9662dd215dcb4591d04ab0b24
wsf <wsf@servyou.com.cn>
2023-03-15 00:22:39 +0800

    Mysql xmind

---
+++ spring-practice-redis-demo/src/test/java/com/wsf/redis/RedisTest.java
@@ -0,0 +1,13 @@
+package com.wsf.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class RedisTest {
+    @Test
+    public void testBitMap() {
+
+    }
+}
